9c2592bddb4e92dc9592417938663cd4cb016e1b
PINNA SAI VARMA <85025248+SAI-VARMA427@users.noreply.github.com>
2024-12-16 18:31:33 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -13,6 +13,8 @@
 # GitHub API URL
 API_URL="https://api.github.com"
 
+helper()
+
 # GitHub username and personal access token
 USERNAME=$username
 TOKEN=$token
@@ -44,6 +46,13 @@ function list_users_with_read_access {
         echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
         echo "$collaborators"
     fi
+}
+
+function helper(){
+    expected_cmd_args=2
+    if [ $# -ne $expected_cmd_args]; then
+    echo "please execute the script with required cmd args"
+    echo "asd"
 }
 
 # Main script
